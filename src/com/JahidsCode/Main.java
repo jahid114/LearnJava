1ad4c5bbca8d897bf0da959b6f36c035de125cc6
MD Zanea Alam Jahid <jahidctgcse@gmail.com>
2024-01-11 15:55:15 +0600

    add: in Main - added comment to separate and identify different section

--- src/com/JahidsCode/Main.java
+++ src/com/JahidsCode/Main.java
@@ -1,10 +1,16 @@
 package com.JahidsCode;
 
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.LongStream;
 
 public class Main {
     public static void main(String[] args) {
+
+        // Testing out the Learn Generic
         /*LearnGeneric<String, Integer> studentData1 = new LearnGeneric<>("jahid", 114);
         System.out.println(studentData1.getFirstData());
         System.out.println(studentData1.getSecondData());
@@ -13,9 +19,14 @@ public class Main {
         System.out.println(studentData2.getFirstData());
         System.out.println(studentData2.getSecondData());*/
 
-        List<Integer> evenNumberUpto100 = LearnStream.generateIntegerList(2,100, 2);
+        // Testing out the Learn Stream
+        /*List<Integer> evenNumberUpto100 = LearnStream.generateIntegerList(2,100, 2);
         List<Integer> multipleOf5Upto100 = LearnStream.generateIntegerList(5,100, 5);
 
-        System.out.println(multipleOf5Upto100.toString());
+        System.out.println(multipleOf5Upto100.toString());*/
+
+
+
+
     }
 }
